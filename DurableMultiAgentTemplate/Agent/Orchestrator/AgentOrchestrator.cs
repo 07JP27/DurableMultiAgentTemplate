69bfca4132f890f19790a3a8e006ca3e1f27c244
Kazuki Ota <kaota@microsoft.com>
2025-01-20 15:42:16 +0900

    全ての activity の呼び出しにリトライポリシーを追加

--- DurableMultiAgentTemplate/Agent/Orchestrator/AgentOrchestrator.cs
+++ DurableMultiAgentTemplate/Agent/Orchestrator/AgentOrchestrator.cs
@@ -1,4 +1,4 @@
-using DurableMultiAgentTemplate.Model;
+﻿using DurableMultiAgentTemplate.Model;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.DurableTask;
 using Microsoft.Extensions.Logging;
@@ -7,6 +7,13 @@ namespace DurableMultiAgentTemplate.Agent.Orchestrator;
 
 public class AgentOrchestrator()
 {
+    private static TaskOptions DefaultTaskOptions { get; } = new(
+        new TaskRetryOptions(new RetryPolicy(
+            3, 
+            TimeSpan.FromSeconds(1),
+            1,
+            TimeSpan.FromSeconds(10))));
+
     [Function(nameof(AgentOrchestrator))]
     public async Task<AgentResponseDto> RunOrchestrator(
         [OrchestrationTrigger] TaskOrchestrationContext context)
@@ -17,7 +24,7 @@ public class AgentOrchestrator()
         ArgumentNullException.ThrowIfNull(reqData);
 
         // AgentDecider呼び出し（呼び出すAgentの決定）
-        var AgentDeciderResult = await context.CallActivityAsync<AgentDeciderResult>(AgentActivityName.AgentDeciderActivity, reqData);
+        var AgentDeciderResult = await context.CallActivityAsync<AgentDeciderResult>(AgentActivityName.AgentDeciderActivity, reqData, DefaultTaskOptions);
 
         // AgentDeciderでエージェントを呼び出さない場合には、そのまま返す
         if (!AgentDeciderResult.IsAgentCall)
@@ -39,7 +46,7 @@ public class AgentOrchestrator()
         foreach (var agentCall in AgentDeciderResult.AgentCalls)
         {
             var args = agentCall.Arguments;
-            parallelAgentCall.Add(context.CallActivityAsync<string>(agentCall.AgentName, args));
+            parallelAgentCall.Add(context.CallActivityAsync<string>(agentCall.AgentName, args, DefaultTaskOptions));
         }
 
         await Task.WhenAll(parallelAgentCall);
@@ -54,12 +61,12 @@ public class AgentOrchestrator()
         
         if (reqData.RequireAdditionalInfo)
         {
-            var res= await context.CallActivityAsync<AgentResponseWithAdditionalInfoDto>(AgentActivityName.SynthesizerWithAdditionalInfoActivity, synthesizerRequest);
+            var res= await context.CallActivityAsync<AgentResponseWithAdditionalInfoDto>(AgentActivityName.SynthesizerWithAdditionalInfoActivity, synthesizerRequest, DefaultTaskOptions);
             return res;
         }
         else
         {
-            var res = await context.CallActivityAsync<AgentResponseDto>(AgentActivityName.SynthesizerActivity, synthesizerRequest);
+            var res = await context.CallActivityAsync<AgentResponseDto>(AgentActivityName.SynthesizerActivity, synthesizerRequest, DefaultTaskOptions);
             return res;
         }
     }
