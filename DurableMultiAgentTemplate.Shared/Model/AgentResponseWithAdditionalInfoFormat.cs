7acca2f527e18479cf081550e79a9c9c4d0a5c4c
07JP27 <jynpei.t@gmail.com>
2025-02-12 13:52:51 +0900

    Delete AgentResponseWithAdditionalInfoFormat.cs

--- DurableMultiAgentTemplate.Shared/Model/AgentResponseWithAdditionalInfoFormat.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-
-namespace DurableMultiAgentTemplate.Shared.Model;
-
-public class AgentResponseWithAdditionalInfoFormat
-{
-    public string Content { get; set; } = "";
-    public List<IAdditionalInfo> AdditionalInfo { get; set; } = new List<IAdditionalInfo>();
-}
